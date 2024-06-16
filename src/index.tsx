b5aa99ba9883dda7c0be87a4407d86bffbbc521e
Paul Obunga <paulobunga.one@gmail.com>
2024-06-16 18:02:56 +0300

    Updated Code

--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,58 @@ const LINKING_ERROR =
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo Go\n';
 
-const BbaRfidScanner = NativeModules.BbaRfidScanner
-  ? NativeModules.BbaRfidScanner
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
+const { C72RFIDScannerModule, AX6737RFIDScannerModule } = NativeModules;
 
-export function multiply(a: number, b: number): Promise<number> {
-  return BbaRfidScanner.multiply(a, b);
+if (!C72RFIDScannerModule || !AX6737RFIDScannerModule) {
+  throw new Error(LINKING_ERROR);
 }
+
+type InitializeReader = () => void;
+type DeinitializeReader = () => void;
+type ReadSingleTag = () => Promise<any>;
+type ReadPower = () => Promise<any>;
+type ChangePower = (powerValue: any) => Promise<any>;
+type AddListener = (cb: (args: any[]) => void) => void;
+type ClearTags = () => void;
+
+type StartReadingTags = () => void;
+type StopReadingTags = () => void;
+type PowerListener = (cb: (power: number) => void) => void;
+type TagListener = (cb: (tag: string) => void) => void;
+
+const C72RFIDScanner = {
+  initializeReader: C72RFIDScannerModule.initializeReader as InitializeReader,
+  deInitializeReader:
+    C72RFIDScannerModule.deInitializeReader as DeinitializeReader,
+  readSingleTag: C72RFIDScannerModule.readSingleTag as ReadSingleTag,
+  readPower: C72RFIDScannerModule.readPower as ReadPower,
+  changePower: C72RFIDScannerModule.changePower as ChangePower,
+  addListener: C72RFIDScannerModule.addListener as AddListener,
+  clearTags: C72RFIDScannerModule.clearTags as ClearTags,
+  startReadingTags: C72RFIDScannerModule.startReadingTags as StartReadingTags,
+  stopReadingTags: C72RFIDScannerModule.stopReadingTags as StopReadingTags,
+  powerListener: C72RFIDScannerModule.powerListener as PowerListener,
+  tagListener: C72RFIDScannerModule.tagListener as TagListener,
+};
+
+const AX6737RFIDScanner = {
+  initializeReader:
+    AX6737RFIDScannerModule.initializeReader as InitializeReader,
+  deInitializeReader:
+    AX6737RFIDScannerModule.deInitializeReader as DeinitializeReader,
+  readSingleTag: AX6737RFIDScannerModule.readSingleTag as ReadSingleTag,
+  readPower: AX6737RFIDScannerModule.readPower as ReadPower,
+  changePower: AX6737RFIDScannerModule.changePower as ChangePower,
+  addListener: AX6737RFIDScannerModule.addListener as AddListener,
+  clearTags: AX6737RFIDScannerModule.clearTags as ClearTags,
+  startReadingTags:
+    AX6737RFIDScannerModule.startReadingTags as StartReadingTags,
+  stopReadingTags: AX6737RFIDScannerModule.stopReadingTags as StopReadingTags,
+  powerListener: AX6737RFIDScannerModule.powerListener as PowerListener,
+  tagListener: AX6737RFIDScannerModule.tagListener as TagListener,
+};
+
+export default {
+  C72RFIDScanner,
+  AX6737RFIDScanner,
+};
