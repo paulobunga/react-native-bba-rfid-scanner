3b3fa38e694faf1581553dadc53c0fd4e66f8a0f
Paul Obunga <paulobunga.one@gmail.com>
2024-06-19 13:38:30 +0300

    Updated Code

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,29 @@
 import { NativeModules, NativeEventEmitter } from 'react-native';
 
+type AddListener = (cb: (args: any[]) => void) => void;
+
 const { C72RFIDScannerModule, AX6737RFIDScannerModule } = NativeModules;
 
 const axEventEmitter = new NativeEventEmitter(AX6737RFIDScannerModule);
 
-const axPowerListener = (listener: any) =>
+const axPowerListener: AddListener = (listener: any) =>
   axEventEmitter.addListener('UHF_POWER', listener);
 
-const axTagListener = (listener: any) =>
+const axTagListener: AddListener = (listener: any) =>
   axEventEmitter.addListener('UHF_TAG', listener);
 
 export const AX6737RFIDScanner = {
   powerListener: axPowerListener,
   tagListener: axTagListener,
-  initialize: AX6737RFIDScannerModule.initializeDevice,
-  startReadingTags: AX6737RFIDScannerModule.startReadingTags,
-  stopReadingTags: AX6737RFIDScannerModule.stopReadingTags,
-  readSingleTag: AX6737RFIDScannerModule.readSingleTag,
-  clearTags: AX6737RFIDScannerModule.clearTags,
+  initialize: () => AX6737RFIDScannerModule.initializeDevice(),
+  startReadingTags: (callback: (args: any[]) => any) =>
+    AX6737RFIDScannerModule.startReadingTags(callback),
+  stopReadingTags: (callback: (args: any[]) => any) =>
+    AX6737RFIDScannerModule.stopReadingTags(callback),
+  readSingleTag: () => AX6737RFIDScannerModule.readSingleTag(),
+  clearTags: () => AX6737RFIDScannerModule.clearTags(),
 };
 
 export const C72RFIDScanner = {
-  initialize: C72RFIDScannerModule.initializeReader,
+  initialize: C72RFIDScannerModule.initializeReader(),
 };
